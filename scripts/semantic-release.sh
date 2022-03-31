ce24ce98acbf85f568b10081c82f469e634ea971
Sam McKelvie <dev@mckelvie.org>
2022-03-31 15:32:22 -0700

    release tweak

--- scripts/semantic-release.sh
+++ scripts/semantic-release.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-PROJECT_DIR="$( dirname "$BIN_DIR" )"
+PROJECT_DIR="$( dirname "$SCRIPT_DIR" )"
 
 cd "$PROJECT_DIR"
 GH_TOKEN="$(cat "$HOME/.private/github-semantic-versioning_token.txt")" semantic-release "$@"
